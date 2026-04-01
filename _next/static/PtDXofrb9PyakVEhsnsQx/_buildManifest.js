609d7d889d9bda3c0e037af04b73f0fc086e32f1
juhs-fl1 <juhs-fl1@users.noreply.github.com>
2026-04-01 13:33:40 +0000

    deploy: juhs-fl1/source@c6ed0e0682a5eb3d8188348fcfd2b44d660736a7

--- _next/static/PtDXofrb9PyakVEhsnsQx/_buildManifest.js
+++ _next/static/PtDXofrb9PyakVEhsnsQx/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/deploy//_next/:path+",
-        "destination": "/deploy/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
